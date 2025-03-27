e86fb80cbf83b451242b16f4f8efdc286a220a95
Ryan Folks <rfolks92@gmail.com>
2025-03-27 19:37:39 -0400

    Create bounding_box.rs with the BoundingBox struct and a constructor.

---
+++ src/annotations/bounding_box.rs
@@ -0,0 +1,44 @@
+use std::fmt;
+
+/// A struct representing a bounding box.
+/// 
+/// A bounding box is a rectangle used to annotate objects in images for training deep object
+/// detection models. An ideal bounding box is the smallest box that totally contains the
+/// object within the image. Bounding boxes are composed of a rectangle and a category denoting
+/// what object it is.
+///
+/// This project uses the standard convention of the left side of the image being x=0 and the top
+/// of the image being y=0.
+#[derive(Debug)]
+pub struct BoundingBox {
+    left: f64,
+    top: f64,
+    right: f64,
+    bottom: f64,
+    category: String
+}
+
+impl BoundingBox {
+    /// Checks if a box has valid parameters before constructing.
+    pub fn new(
+        left: f64, top: f64, right: f64, bottom: f64, category: String
+    ) -> Result<Self, String> {
+        if left > right {
+            Err(format!(
+                "Failed to create BoundingBox, value for left > value for right ({} > {})",
+                left,
+                right
+            ))
+        } else if top > bottom {
+            Err(format!(
+                "Failed to create BoundingBox, value for top > value for bottom ({} > {}).",
+                top,
+                bottom
+            ))
+        } else {
+            Ok(BoundingBox {left, top, right, bottom, category})
+        }
+    }
+}
+
+pub trait BoundingBoxOperations {}
