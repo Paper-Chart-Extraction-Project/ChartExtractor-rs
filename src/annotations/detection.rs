ba4f5ea7690f351b8ed32522b9ed009b76a342bc
Ryan Folks <rfolks92@gmail.com>
2025-03-28 10:15:23 -0400

    Implement Display for Detection struct.

--- src/annotations/detection.rs
+++ src/annotations/detection.rs
@@ -1,11 +1,20 @@
 use crate::annotations::bounding_box::{BoundingBoxGeometry};
+use std::fmt;
 
 /// A detection is what is produced as output from an object detection model.
 ///
 /// A detection is any annotation combined with a confidence score: a probability value that
 /// encodes the model's belief that the detection is true.
 #[derive(Debug)]
-pub struct Detection<T: BoundingBoxGeometry> {
+pub struct Detection<T: BoundingBoxGeometry + fmt::Display> {
     pub annotation: T,
     pub confidence: f64
+}
+
+impl<T: BoundingBoxGeometry + fmt::Display> fmt::Display for Detection<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f, "Detection {{ annotation: {}, confidence: {} }}", self.annotation, self.confidence
+        )
+    }
 }
