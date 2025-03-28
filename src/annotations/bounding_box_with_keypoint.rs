ac56c03963963e1f04b621bae81fa55494e536af
Ryan Folks <rfolks92@gmail.com>
2025-03-28 07:41:28 -0400

    Correct some small errors.

--- src/annotations/bounding_box_with_keypoint.rs
+++ src/annotations/bounding_box_with_keypoint.rs
@@ -1,5 +1,5 @@
-use annotations::bounding_box::{BoundingBox};
-use annotations::point::{Point};
+use crate::annotations::bounding_box::{BoundingBox};
+use crate::annotations::point::{Point};
 
 /// A struct representing a BoundingBox + Keypoint annotation.
 ///
@@ -7,7 +7,7 @@ use annotations::point::{Point};
 /// to place keypoints into the frame as well. Therefore, the output of pose models is both a
 /// bounding box as well as a list of points relating to the "pose" of the object. For this project
 /// we only have pose models that predict a single keypoint.
-#[Derive(Debug)]
+#[derive(Debug)]
 pub struct BoundingBoxWithKeypoint {
     bounding_box: BoundingBox,
     keypoint: Point
@@ -22,11 +22,21 @@ impl BoundingBoxWithKeypoint {
         keypoint_x: f64,
         keypoint_y: f64,
         category: String
-    ) -> Result<BoundingBoxWithKeypoint, String> {}
+    ) -> Result<BoundingBoxWithKeypoint, String> {
+        let bbox: Result<BoundingBox, String> = BoundingBox::new(
+            left, top, right, bottom, category
+        );
+        let bounding_box: BoundingBox = match bbox {
+            Ok(b) => b,
+            Err(s) => return Err(s)
+        };
+        let keypoint: Point = Point {x: keypoint_x, y: keypoint_y};
+        Ok(BoundingBoxWithKeypoint {bounding_box, keypoint})
+    }
 
-    pub fn left() -> f64 {}
-    pub fn top() -> f64 {}
-    pub fn right() -> f64 {}
-    pub fn bottom() -> f64 {}
-    pub fn category() -> f64 {}
+    pub fn left(&self) -> f64 {0_f64}
+    pub fn top(&self) -> f64 {0_f64}
+    pub fn right(&self) -> f64 {0_f64}
+    pub fn bottom(&self) -> f64 {0_f64}
+    pub fn category(&self) -> &str {&""}
 }
