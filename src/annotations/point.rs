ffd70e87bfa21c226cca9a92d6a0cf241e01f38d
Ryan Folks <rfolks92@gmail.com>
2025-03-28 10:01:22 -0400

    Add Display trait to Point.

--- src/annotations/point.rs
+++ src/annotations/point.rs
@@ -1,6 +1,14 @@
+use std::fmt;
+
 /// A struct representing a simple point.
 #[derive(Debug)]
 pub struct Point {
     pub x: f64,
     pub y: f64
+}
+
+impl fmt::Display for Point {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Point {{ x: {}, y: {} }}", self.x, self.y)
+    }
 }
