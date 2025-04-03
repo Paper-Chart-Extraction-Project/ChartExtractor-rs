1652b34b73585414137843f1be38df2b74a0de3d
RyanDoesMath <rfolks92@gmail.com>
2025-04-03 07:25:15 -0400

    Add image_utils module and a function to read an image as an array.

---
+++ src/image_utils/image_io.rs
@@ -0,0 +1,17 @@
+use image;
+use ndarray::{Array, ArrayBase, Dim, OwnedRepr};
+use std::path::Path;
+
+pub fn read_image_as_array4(filepath: &Path) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>> {
+    let img = image::open(filepath).unwrap().to_rgb8();
+    let mut input = Array::zeros((1, 3, img.width() as usize, img.height() as usize));
+    for pixel in img.enumerate_pixels() {
+        let x = pixel.0 as _;
+        let y = pixel.1 as _;
+        let [r, g, b] = pixel.2.0;
+        input[[0, 0, y, x]] = (r as f32) / 255.;
+        input[[0, 0, y, x]] = (g as f32) / 255.;
+        input[[0, 0, y, x]] = (b as f32) / 255.;
+    }
+    input
+}
