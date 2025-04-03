86f1d7f3433f5d953972b19e86e21a9ec039857c
RyanDoesMath <rfolks92@gmail.com>
2025-04-03 07:44:04 -0400

    Added some error checking in main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,24 @@ mod image_utils;
 use image_utils::image_io::read_image_as_array4;
 use ndarray::s;
 use object_detection::yolov11::{read_classes_txt_file, Yolov11};
+use std::error::Error;
 use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let model_path = Path::new("./data/models/yolo11n.onnx");
     let classes_path = Path::new("./data/model_metadata/coco-classes.txt");
-    let yv11 = Yolov11::new(
+
+    if !model_path.exists() {
+        return Err(
+            format!("Model path does not exist, or cannot be read: {:?}", model_path).into()
+        );
+    }
+    if !classes_path.exists() {
+        return Err(
+            format!("Classes path does not exist, or cannot be read: {:?}", classes_path).into()
+        );
+    }
+    let model = Yolov11::new(
         model_path,
         read_classes_txt_file(classes_path).unwrap(),
         640,
@@ -17,6 +29,9 @@ fn main() {
         "yolov11n onnx".to_string()
     ).unwrap();
     let img = read_image_as_array4(Path::new("./data/images/test.jpg"));
+    // This implementation only takes views into arrays as input.
+    // Check yolov11.rs for detail.
     let view_into_img = img.slice(s![.., .., 0..640, 0..640]);
-    println!("Preds: {:?}", yv11.run_inference(view_into_img, 0.5_f32));
+    println!("Preds: {:?}", model.run_inference(view_into_img, 0.5_f32));
+    Ok(())
 }
