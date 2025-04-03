05677900103ce92c99284b67b9fccea8e5592187
RyanDoesMath <rfolks92@gmail.com>
2025-04-03 07:11:02 -0400

    Add some documentation to run_inference.

--- src/object_detection/yolov11.rs
+++ src/object_detection/yolov11.rs
@@ -39,6 +39,16 @@ impl Yolov11 {
         Ok(Yolov11{ort_session, class_names, input_width, input_height, model_name})
     }
 
+    /// This method does not take an array directly, but rather a view into an array.
+    /// For our use case, we will be doing a lot of cropping images by making views
+    /// into ndarray objects. The purpose of this is to be able to do tiled detection
+    /// on an image without making copies.
+    ///
+    /// image.slice(s![.., .., start_row..end_row, start_col..end_col])
+    /// where image is an ndarray with dimensions (1, 3, image_width, image_height).
+    ///
+    /// If you want to reuse this and skip the view making process, changing ViewRepr<f32>
+    /// to OwnedRepr<f32> will likely work.
     pub fn run_inference(
         &self, input_array: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>>, confidence: f32
     ) -> Vec<Detection<BoundingBox>> {
