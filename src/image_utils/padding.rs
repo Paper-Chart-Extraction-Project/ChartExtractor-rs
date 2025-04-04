85ad798f74a3b14b144b8272548e4b005333a41f
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 14:47:22 -0400

    Add errors for when padding fails.
    If the new width or new height is smaller than the original image's width or height it now returns an error.

--- src/image_utils/padding.rs
+++ src/image_utils/padding.rs
@@ -1,9 +1,63 @@
 use image::{Rgb, RgbImage};
+use std::fmt;
+
+/// A set of custom errors for more informative error handling.
+#[derive(Debug)]
+pub enum ImagePaddingError {
+    InvalidWidth {original_width: u32, new_width: u32},
+    InvalidHeight {original_height: u32, new_height: u32},
+    InvalidDimensions {original_width: u32, new_width: u32, original_height: u32, new_height: u32}
+}
+
+impl fmt::Display for ImagePaddingError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ImagePaddingError::InvalidWidth{original_width, new_width} => {
+                write!(
+                    f,
+                    "Failed to pad image, new width ({}) < original width ({}).",
+                    new_width, original_width
+                )
+            },
+            ImagePaddingError::InvalidHeight{original_height, new_height} => {
+                write!(
+                    f,
+                    "Failed to pad image, new height ({}) < original height ({}).",
+                    new_height, original_height
+                )
+            }
+            ImagePaddingError::InvalidDimensions{
+                original_width, new_width, original_height, new_height
+            } => {
+                write!(
+                    f,
+                    "Failed to pad image, new width ({}) < original width ({}) \
+                    and new height({}) < original height({})",
+                    new_width, original_width, new_height, original_height
+                )
+            }
+        }
+    }
+}
+
 
 /// Pads an rgb8 image by adding pixels to the right and bottom of the image.
 pub fn pad_right_bottom_img_rbg8(
     original_image: RgbImage, new_width: u32, new_height: u32
-) -> RgbImage {
+) -> Result<RgbImage, ImagePaddingError> {
+    let (original_width, original_height) = original_image.dimensions();
+    let new_width_is_too_small = new_width < original_width;
+    let new_height_is_too_small = new_height < original_height;
+    if new_width_is_too_small && new_height_is_too_small {
+        return Err(ImagePaddingError::InvalidDimensions {
+            original_width, new_width, original_height, new_height
+        });
+    } else if new_width_is_too_small {
+        return Err(ImagePaddingError::InvalidWidth {original_width, new_width});
+    } else if new_height_is_too_small {
+        return Err(ImagePaddingError::InvalidHeight {original_height, new_height});
+    }
+
     let mut padded_image: RgbImage = RgbImage::new(new_width, new_height);
     for pixel in original_image.enumerate_pixels() {
         let x = pixel.0;
@@ -11,5 +65,5 @@ pub fn pad_right_bottom_img_rbg8(
         let [r, g, b] = original_image.get_pixel_checked(x, y).unwrap().0;
         padded_image.put_pixel(x, y, Rgb([r, g, b]));
     }
-    padded_image
+    Ok(padded_image)
 }
