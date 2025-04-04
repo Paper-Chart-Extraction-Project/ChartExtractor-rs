51f3bac586096990b3cde768b8b8ad9c796534af
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 17:25:54 -0400

    Add a fn to read images as an RgbImage.
    Also format image_io.rs

--- src/image_utils/image_io.rs
+++ src/image_utils/image_io.rs
@@ -1,9 +1,9 @@
-use image;
+use image::{self, RgbImage};
 use ndarray::{Array, ArrayBase, Dim, OwnedRepr};
 use std::path::Path;
 
 pub fn read_image_as_array4(filepath: &Path) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>> {
-    let img = image::open(filepath).unwrap().to_rgb8();
+    let img = image::open(filepath).unwrap().into_rgb8();
     let mut input = Array::zeros((1, 3, img.width() as usize, img.height() as usize));
     for pixel in img.enumerate_pixels() {
         let x = pixel.0 as _;
@@ -14,4 +14,8 @@ pub fn read_image_as_array4(filepath: &Path) -> ArrayBase<OwnedRepr<f32>, Dim<[u
         input[[0, 0, y, x]] = (b as f32) / 255.;
     }
     input
+}
+
+pub fn read_image_as_rgb8(filepath: &Path) -> RgbImage {
+    image::open(filepath).unwrap().into_rgb8()
 }
