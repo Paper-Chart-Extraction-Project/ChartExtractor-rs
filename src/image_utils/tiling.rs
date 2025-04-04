3f29700d7d44ba993e502fd713e4d2381c5a8d82
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 17:54:43 -0400

    Add errors and an enum for overlap ratios.
    Going with an enum for this because it will circumvent float->int conversion errors when tiling.

--- src/image_utils/tiling.rs
+++ src/image_utils/tiling.rs
@@ -3,18 +3,22 @@ use std::fmt;
 /// A set of custom errors for more informative error handling.
 #[derive(Debug)]
 pub enum TilingError {
-    InvalidWidth {
-        original_width: u32,
-        new_width: u32,
+    InvalidTileSize {
+        tile_width: u32,
+        tile_height: u32,
+        image_width: u32
+        image_height: u32,
     },
-    InvalidHeight {
-        original_height: u32,
-        new_height: u32,
-    },
-    InvalidDimensions {
-        original_width: u32,
-        new_width: u32,
-        original_height: u32,
-        new_height: u32,
-    },
+    InvalidOverlapRatio {
+        image_width: u32,
+        image_height: u32,
+        overlap_ratio: OverlapRatio
+    }
+}
+
+pub enum OverlapRatio {
+    ONE_HALF,
+    ONE_THIRD,
+    ONE_FOURTH,
+    ONE_FIFTH
 }
