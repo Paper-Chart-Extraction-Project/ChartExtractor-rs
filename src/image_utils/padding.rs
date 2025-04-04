aebbc1a0fdcc4c916556221b0472455411a3e7e7
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 14:13:45 -0400

    Add function to pad images by adding black pixels to the right and bottom.

--- src/image_utils/padding.rs
+++ src/image_utils/padding.rs
@@ -1,3 +1,15 @@
-use image;
+use image::{Rgb, RgbImage};
 
-
+/// Pads an rgb8 image by adding pixels to the right and bottom of the image.
+pub fn pad_right_bottom_img_rbg8(
+    original_image: RgbImage, new_width: u32, new_height: u32
+) -> RgbImage {
+    let mut padded_image: RgbImage = RgbImage::new(new_width, new_height);
+    for pixel in original_image.enumerate_pixels() {
+        let x = pixel.0;
+        let y = pixel.1;
+        let [r, g, b] = original_image.get_pixel_checked(x, y).unwrap().0;
+        padded_image.put_pixel(x, y, Rgb([r, g, b]));
+    }
+    padded_image
+}
