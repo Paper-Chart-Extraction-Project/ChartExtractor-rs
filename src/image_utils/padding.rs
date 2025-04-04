71cdc89edb70b16a95bc718e8d42a3fe247649ee
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 15:07:36 -0400

    Extract validation into separate function.

--- src/image_utils/padding.rs
+++ src/image_utils/padding.rs
@@ -40,22 +40,33 @@ impl fmt::Display for ImagePaddingError {
     }
 }
 
+fn validate_padding_parameters(
+    original_width: u32, original_height: u32, new_width: u32, new_height: u32
+) -> Option<ImagePaddingError> {
+    let new_width_is_too_small = new_width < original_width;
+    let new_height_is_too_small = new_height < original_height;
+    if new_width_is_too_small && new_height_is_too_small {
+        return Some(ImagePaddingError::InvalidDimensions {
+            original_width, new_width, original_height, new_height
+        });
+    } else if new_width_is_too_small {
+        return Some(ImagePaddingError::InvalidWidth {original_width, new_width});
+    } else if new_height_is_too_small {
+        return Some(ImagePaddingError::InvalidHeight {original_height, new_height});
+    }
+    None
+}
 
 /// Pads an rgb8 image by adding pixels to the right and bottom of the image.
 pub fn pad_right_bottom_img_rbg8(
     original_image: RgbImage, new_width: u32, new_height: u32
 ) -> Result<RgbImage, ImagePaddingError> {
     let (original_width, original_height) = original_image.dimensions();
-    let new_width_is_too_small = new_width < original_width;
-    let new_height_is_too_small = new_height < original_height;
-    if new_width_is_too_small && new_height_is_too_small {
-        return Err(ImagePaddingError::InvalidDimensions {
-            original_width, new_width, original_height, new_height
-        });
-    } else if new_width_is_too_small {
-        return Err(ImagePaddingError::InvalidWidth {original_width, new_width});
-    } else if new_height_is_too_small {
-        return Err(ImagePaddingError::InvalidHeight {original_height, new_height});
+    let params_are_valid = validate_padding_parameters(
+        original_width, original_height, new_width, new_height
+    );
+    if let Some(e) = params_are_valid {
+        return Err(e);
     }
 
     let mut padded_image: RgbImage = RgbImage::new(new_width, new_height);
