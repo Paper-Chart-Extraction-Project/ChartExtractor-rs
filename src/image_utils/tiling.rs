cae6e431739591744bcd73f5a1c57d03de2c7e40
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 17:43:01 -0400

    Add a custom error for tiling.

--- src/image_utils/tiling.rs
+++ src/image_utils/tiling.rs
@@ -1,1 +1,20 @@
 use std::fmt;
+
+/// A set of custom errors for more informative error handling.
+#[derive(Debug)]
+pub enum TilingError {
+    InvalidWidth {
+        original_width: u32,
+        new_width: u32,
+    },
+    InvalidHeight {
+        original_height: u32,
+        new_height: u32,
+    },
+    InvalidDimensions {
+        original_width: u32,
+        new_width: u32,
+        original_height: u32,
+        new_height: u32,
+    },
+}
