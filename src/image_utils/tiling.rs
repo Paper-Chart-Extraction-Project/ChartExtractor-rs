e79e9ca22d594f50da85a88c261ec2ed28b66b0a
RyanDoesMath <rfolks92@gmail.com>
2025-04-04 20:20:59 -0400

    Add a function to validatate tiling parameters.

--- src/image_utils/tiling.rs
+++ src/image_utils/tiling.rs
@@ -4,21 +4,58 @@ use std::fmt;
 #[derive(Debug)]
 pub enum TilingError {
     InvalidTileSize {
-        tile_width: u32,
-        tile_height: u32,
-        image_width: u32
-        image_height: u32,
-    },
-    InvalidOverlapRatio {
+        tile_size: u32,
         image_width: u32,
         image_height: u32,
-        overlap_ratio: OverlapRatio
+    },
+    IncompatibleProportionWithTileSize {
+        tile_size: u32,
+        overlap_proportion: OverlapProportion
+    },
+    UnevenImageDivision {
+        image_height: u32,
+        image_width: u32,
+        tile_size: u32,
+        overlap_proportion: OverlapProportion
     }
 }
 
-pub enum OverlapRatio {
-    ONE_HALF,
-    ONE_THIRD,
-    ONE_FOURTH,
-    ONE_FIFTH
+#[derive(Debug, Clone, Copy)]
+pub enum OverlapProportion {
+    ONE_HALF = 2,
+    ONE_THIRD = 3,
+    ONE_FOURTH = 4,
+    ONE_FIFTH = 5
+}
+
+pub fn validate_tiling_parameters(
+    proportion: OverlapProportion,
+    tile_size: u32,
+    image_width: u32,
+    image_height: u32
+) -> Option<TilingError> {
+    if tile_size > image_width || tile_size > image_height {
+        return Some(TilingError::InvalidTileSize {tile_size, image_width, image_height});
+    }
+
+    let tile_cleanly_divides = tile_size % (proportion as u32) == 0;
+    if !tile_cleanly_divides {
+        return Some(TilingError::IncompatibleProportionWithTileSize {
+            tile_size,
+            overlap_proportion: proportion
+        });
+    }
+    
+    let tiles_fit_cleanly_laterally = image_width % (tile_size / (proportion as u32)) == 0;
+    let tiles_fit_cleanly_vertically = image_height % (tile_size / (proportion as u32)) == 0;
+    
+    if !tiles_fit_cleanly_laterally || !tiles_fit_cleanly_vertically {
+        return Some(TilingError::UnevenImageDivision {
+            image_height,
+            image_width,
+            tile_size,
+            overlap_proportion: proportion
+        })
+    }
+    None
 }
