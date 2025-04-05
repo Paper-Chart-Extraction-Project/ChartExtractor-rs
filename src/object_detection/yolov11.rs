6cb2a1174f99d8e2e73f04e192d99f47c439dcd7
RyanDoesMath <rfolks92@gmail.com>
2025-04-05 14:26:31 -0400

    Cargo fmt yolov11.rs

--- src/object_detection/yolov11.rs
+++ src/object_detection/yolov11.rs
@@ -1,6 +1,6 @@
 use crate::annotations::bounding_box::BoundingBox;
 use crate::annotations::detection::Detection;
-use ndarray::{Array4, ArrayBase, Axis, Dim, s, ViewRepr};
+use ndarray::{Array4, ArrayBase, Axis, Dim, ViewRepr, s};
 use ort::inputs;
 use ort::session::{Session, SessionOutputs};
 use std::fs::File;
@@ -9,13 +9,13 @@ use std::path::Path;
 
 /// An onnxruntime inference session.
 pub struct OrtInferenceSession {
-    session: Session
+    session: Session,
 }
 
 impl OrtInferenceSession {
     pub fn new(model_path: &Path) -> ort::Result<Self> {
         let session = Session::builder()?.commit_from_file(model_path)?;
-        Ok(Self{session})
+        Ok(Self { session })
     }
 }
 
@@ -24,7 +24,7 @@ pub struct Yolov11 {
     class_names: Vec<String>,
     input_width: usize,
     input_height: usize,
-    model_name: String
+    model_name: String,
 }
 
 impl Yolov11 {
@@ -33,10 +33,16 @@ impl Yolov11 {
         class_names: Vec<String>,
         input_width: usize,
         input_height: usize,
-        model_name: String
+        model_name: String,
     ) -> ort::Result<Self> {
         let ort_session = OrtInferenceSession::new(model_path)?;
-        Ok(Yolov11{ort_session, class_names, input_width, input_height, model_name})
+        Ok(Yolov11 {
+            ort_session,
+            class_names,
+            input_width,
+            input_height,
+            model_name,
+        })
     }
 
     /// This method does not take an array directly, but rather a view into an array.
@@ -50,11 +56,15 @@ impl Yolov11 {
     /// If you want to reuse this and skip the view making process, changing ViewRepr<f32>
     /// to OwnedRepr<f32> will likely work.
     pub fn run_inference(
-        &self, input_array: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>>, confidence: f32
+        &self,
+        input_array: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>>,
+        confidence: f32,
     ) -> Vec<Detection<BoundingBox>> {
-        let outputs: SessionOutputs = self.ort_session.session.run(
-            inputs!["images" => input_array].unwrap()
-        ).unwrap();
+        let outputs: SessionOutputs = self
+            .ort_session
+            .session
+            .run(inputs!["images" => input_array].unwrap())
+            .unwrap();
         let output = outputs["output0"].try_extract_tensor::<f32>().unwrap();
         let output = output.t();
 
@@ -66,21 +76,30 @@ impl Yolov11 {
                 .skip(4) // skips bounding box coords.
                 .enumerate()
                 .map(|(index, value)| (index, *value))
-                .reduce(|accum, row| if row.1 > accum.1 {row} else {accum})
+                .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
                 .unwrap();
             if prob < confidence {
                 continue;
             }
             let label = match self.class_names.get(class_id) {
                 Some(v) => v,
-                None => &class_id.to_string()
+                None => &class_id.to_string(),
             };
             let x = row[0];
             let y = row[1];
             let w = row[2];
             let h = row[3];
-            let bbox = BoundingBox::new(x - (w/2.0), y - (h/2.0), x + (w/2.0), y + (y/2.0), label.to_string());
-            detections.push(Detection {annotation: bbox.unwrap(), confidence: prob});
+            let bbox = BoundingBox::new(
+                x - (w / 2.0),
+                y - (h / 2.0),
+                x + (w / 2.0),
+                y + (y / 2.0),
+                label.to_string(),
+            );
+            detections.push(Detection {
+                annotation: bbox.unwrap(),
+                confidence: prob,
+            });
         }
         detections
     }
