96d09976671fe008bc605314bbcb5dded3f75869
Ryan Folks <rfolks92@gmail.com>
2025-04-10 13:47:01 -0400

    Add function to perform the homography transformation on an image given source and destination points.

--- src/image_transformation/homography.rs
+++ src/image_transformation/homography.rs
@@ -1,6 +1,17 @@
 use crate::annotations::point::Point;
-use imageproc::geometric_transformations::Projection;
+use image::{Rgb, RgbImage};
+use imageproc::geometric_transformations::{Interpolation, Projection, warp};
 
+
+pub fn homography_transform(
+    image: RgbImage,
+    source_points: Vec<Point>,
+    destination_points: Vec<Point>
+) -> Option<RgbImage> {
+    let proj: Projection = compute_homography_projection(source_points, destination_points)?;
+    let default_pixel_color: Rgb<u8> = Rgb([0, 0, 0]);
+    Some(warp(&image, &proj, Interpolation::Bilinear, default_pixel_color))
+}
 
 /// Computes the homography transformation.
 ///
@@ -10,7 +21,7 @@ use imageproc::geometric_transformations::Projection;
 /// the coordinates of a perfect, scan-like version of the image.
 ///
 /// source_points and destination_points must be exactly four points.
-pub fn compute_homography_transformation(
+pub fn compute_homography_projection(
     source_points: Vec<Point>,
     destination_points: Vec<Point>
 ) -> Option<Projection> {
