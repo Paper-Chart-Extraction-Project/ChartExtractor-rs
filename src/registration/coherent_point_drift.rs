c083b79e1f5b87c7c2595e092dec2203b61eafb1
Ryan Folks <rfolks92@gmail.com>
2025-04-25 13:25:33 -0400

    Port the pycpd project's DeformableRegistration class to Rust.
    I don't love the object oriented design, but it is clear why it was done this way. It does make it harder to test though.

---
+++ src/registration/coherent_point_drift.rs
@@ -0,0 +1,245 @@
+extern crate openblas_src;
+
+use itertools::Itertools;
+use ndarray::{Array, ArrayBase, Axis, Dim, OwnedRepr, s, stack};
+use ndarray_linalg::Solve;
+use std::f32::consts::PI;
+use std::f32::EPSILON;
+
+struct CoherentPointDriftTransform {
+    X: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Y: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    alpha: f32,
+    beta: f32,
+    TY: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    sigma2: f32,
+    N: usize,
+    M: usize,
+    D: usize,
+    tolerance: f32,
+    w: f32,
+    max_iterations: u32,
+    iteration: u32,
+    diff: f32,
+    q: f32,
+    P: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Pt1: ArrayBase<OwnedRepr<f32>, Dim<[usize; 1]>>,
+    P1: ArrayBase<OwnedRepr<f32>, Dim<[usize; 1]>>,
+    PX: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Np: f32,
+    W: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    G: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    num_eig: u32,
+    history: Vec<String>,
+    debug: bool
+}
+
+impl CoherentPointDriftTransform {
+    pub fn new(
+        X: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+        Y: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+        alpha: f32,
+        beta: f32,
+        w: Option<f32>,
+        tolerance: Option<f32>,
+        max_iterations: Option<u32>,
+        num_eig: Option<u32>,
+        debug: Option<bool>
+    ) -> CoherentPointDriftTransform {
+        let num_target_points: usize = X.dim().0;
+        let dimensions: usize = X.dim().1;
+        let num_source_points: usize = Y.dim().0;
+        let tolerance = match tolerance {
+            Some(v) => v,
+            None => 0.001
+        };
+        let max_iterations = match max_iterations {
+            Some(v) => v,
+            None => 100
+        };
+        let w = match w {
+            Some(v) => v,
+            None => 0.0
+        };
+        let num_eig = match num_eig {
+            Some(v) => v,
+            None => 100
+        };
+        let debug = match debug {
+            Some(v) => v,
+            None => false
+        };
+        CoherentPointDriftTransform {
+            X: X.clone(),
+            Y: Y.clone(),
+            alpha: alpha,
+            beta: beta,
+            TY: Y.clone(),
+            sigma2: initialize_sigma2(&X, &Y),
+            N: num_target_points,
+            M: num_source_points,
+            D: dimensions,
+            tolerance: tolerance,
+            w: w,
+            max_iterations: max_iterations,
+            iteration: 0,
+            diff: f32::MAX,
+            q: f32::MAX,
+            P: Array::zeros((num_source_points, num_target_points)),
+            Pt1: Array::zeros(num_target_points),
+            P1: Array::zeros(num_source_points),
+            PX: Array::zeros((num_source_points, dimensions)),
+            Np: 0.0,
+            W: Array::zeros((num_source_points, dimensions)),
+            G: gaussian_kernel(&Y, &Y, beta),
+            num_eig: num_eig,
+            history: Vec::new(),
+            debug: debug
+        }
+    }
+
+    pub fn register(&mut self) {
+        self.transform_point_cloud();
+        while self.iteration < self.max_iterations && self.diff > self.tolerance {
+            if self.debug {
+                self.history.push(
+                    format!("\"{}\": {}", self.iteration, array_to_string(&self.TY))
+                );
+            }
+            self.iterate();
+        }
+    }
+    
+    fn transform_point_cloud(&mut self) {
+        self.update_transform();
+        let G = gaussian_kernel(&self.Y, &self.Y, self.beta);
+        self.TY = self.Y.clone() + self.G.clone().dot(&self.W.clone());
+    }
+    
+    fn iterate(&mut self) {
+        self.expectation();
+        self.maximization();
+        self.iteration += 1;
+    }
+
+    fn expectation(&mut self) {
+        let mut P = ((compute_diff(&self.X, &self.TY)).powi(2)).sum_axis(Axis(2));
+        P = (-P/(2_f32*self.sigma2)).exp();
+        let c = {
+            let left = (2.0*PI*self.sigma2).powf((self.D as f32)/2.0);
+            let right = self.w/(1.0-self.w)*(self.M as f32)/(self.N as f32);
+            left * right
+        };
+        let mut den = P.sum_axis(Axis(0));
+        den = den.mapv(|v| if v == 0.0 {f32::EPSILON + c} else {v + c});
+
+        self.P = P.clone()/den;
+        self.Pt1 = self.P.clone().sum_axis(Axis(0));
+        self.P1 = self.P.clone().sum_axis(Axis(1));
+        self.Np = self.P1.sum();
+        self.PX = self.P.clone().dot(&self.X);
+    }
+
+    fn maximization(&mut self) {
+        self.update_transform();
+        self.transform_point_cloud();
+        self.update_variance();
+    }
+
+    fn update_transform(&mut self) {
+        let A = {
+            let left_term = Array::from_diag(&self.P1.clone()).dot(&self.G.clone());
+            let right_term = self.alpha * self.sigma2 * Array::eye(self.M.clone());
+            left_term + right_term
+        };
+        let B = self.PX.clone() - Array::from_diag(&self.P1.clone()).dot(&self.Y.clone());
+        self.W = solve_matrices(&A, &B);
+    }
+
+    fn update_variance(&mut self) { 
+        let qprev = self.sigma2;
+        self.q = f32::MAX;
+        let xPx = self.Pt1.clone().t().dot(&self.X.clone().powi(2).sum_axis(Axis(1)));
+        let yPy = self.P1.clone().t().dot(&self.TY.clone().powi(2).sum_axis(Axis(1)));
+        let trPXY = (self.TY.clone()*self.PX.clone()).sum();
+
+        self.sigma2 = (xPx - 2.0 * trPXY + yPy) / (self.Np * self.D as f32);
+        if self.sigma2 <= 0.0 {
+            self.sigma2 = self.tolerance / 10.0;
+        }
+        self.diff = (self.sigma2 - qprev).abs();
+    }
+}
+
+
+// Non cpd functions.
+fn compute_diff(
+    X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>
+) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> {
+    let X_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> = Array::from_shape_vec(
+        (1, X.dim().0, X.dim().1),
+        X.clone().into_raw_vec()
+    ).unwrap(); 
+    let Y_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> = Array::from_shape_vec(
+        (Y.dim().0, 1, Y.dim().1),
+        Y.clone().into_raw_vec()
+    ).unwrap();
+    X_3d - Y_3d
+}
+
+fn gaussian_kernel(
+    X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    beta: f32
+) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>> {
+    let X = X.clone();
+    let Y = Y.clone();
+    let diff = compute_diff(&X, &Y).powi(2).sum_axis(Axis(2));
+    (-diff/(2.0 * beta.powi(2))).exp()
+}
+
+fn initialize_sigma2(
+    X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>
+) -> f32 {
+    let N = X.dim().0 as f32;
+    let D = X.dim().1 as f32;
+    let M = Y.dim().0 as f32;
+    let diff = compute_diff(X, Y);
+    let err = diff.powi(2);
+    err.sum()/(D*N*M)
+}
+
+fn array_to_string(arr: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>) -> String {
+    let mut arr_str = String::from("[");
+    let arr = arr.clone();
+    let chunks = arr.into_iter().chunks(2);
+    let mut p_str: Vec<String> = Vec::new();
+    for chunk in &chunks {
+        let p: Vec<f32> = chunk.collect::<Vec<f32>>();
+        p_str.push(String::from(format!("{{\"x\": {}, \"y\": {}}}", p[0], p[1])));
+    }
+    let p_str: String = p_str.join(", ");
+    arr_str.push_str(&p_str);
+
+    arr_str.push_str("]");
+    arr_str
+}
+
+fn solve_matrices(
+    A: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+    B: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
+) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>> {
+    let A = A.clone();
+    let B = B.clone();
+    let num_cols = B.dim().1;
+    let mut solutions: Vec<_> = Vec::new();
+    for c in 0..num_cols {
+        let col = B.slice(s![.., c]).to_owned();
+        let soln = A.solve_into(col).unwrap();
+        solutions.push(soln);
+    }
+    let solutions = solutions.iter().map(|x| x.view()).collect::<Vec<_>>();
+    stack(Axis(1), &solutions[..]).unwrap()
+}
