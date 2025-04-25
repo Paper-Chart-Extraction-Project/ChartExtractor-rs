7abd721e272935e758256b8bdde6d936b8de0a8b
Ryan Folks <rfolks92@gmail.com>
2025-04-25 14:09:21 -0400

    Format coherent_point_drift.rs.

--- src/registration/coherent_point_drift.rs
+++ src/registration/coherent_point_drift.rs
@@ -3,8 +3,8 @@ extern crate openblas_src;
 use itertools::Itertools;
 use ndarray::{Array, ArrayBase, Axis, Dim, OwnedRepr, s, stack};
 use ndarray_linalg::Solve;
-use std::f32::consts::PI;
 use std::f32::EPSILON;
+use std::f32::consts::PI;
 
 struct CoherentPointDriftTransform {
     X: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
@@ -31,7 +31,7 @@ struct CoherentPointDriftTransform {
     G: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
     num_eig: u32,
     history: Vec<String>,
-    debug: bool
+    debug: bool,
 }
 
 impl CoherentPointDriftTransform {
@@ -44,7 +44,7 @@ impl CoherentPointDriftTransform {
         tolerance: Option<f32>,
         max_iterations: Option<u32>,
         num_eig: Option<u32>,
-        debug: Option<bool>
+        debug: Option<bool>,
     ) -> CoherentPointDriftTransform {
         let num_target_points: usize = X.dim().0;
         let dimensions: usize = X.dim().1;
@@ -74,7 +74,7 @@ impl CoherentPointDriftTransform {
             G: gaussian_kernel(&Y, &Y, beta),
             num_eig: num_eig.unwrap_or(100),
             history: Vec::new(),
-            debug: debug.unwrap_or(false)
+            debug: debug.unwrap_or(false),
         }
     }
 
@@ -82,20 +82,22 @@ impl CoherentPointDriftTransform {
         self.transform_point_cloud();
         while self.iteration < self.max_iterations && self.diff > self.tolerance {
             if self.debug {
-                self.history.push(
-                    format!("\"{}\": {}", self.iteration, array_to_string(&self.TY))
-                );
+                self.history.push(format!(
+                    "\"{}\": {}",
+                    self.iteration,
+                    array_to_string(&self.TY)
+                ));
             }
             self.iterate();
         }
     }
-    
+
     fn transform_point_cloud(&mut self) {
         self.update_transform();
         let G = gaussian_kernel(&self.Y, &self.Y, self.beta);
         self.TY = self.Y.clone() + self.G.clone().dot(&self.W.clone());
     }
-    
+
     fn iterate(&mut self) {
         self.expectation();
         self.maximization();
@@ -104,16 +106,17 @@ impl CoherentPointDriftTransform {
 
     fn expectation(&mut self) {
         let mut P = ((compute_diff(&self.X, &self.TY)).powi(2)).sum_axis(Axis(2));
-        P = (-P/(2_f32*self.sigma2)).exp();
+        P = (-P / (2_f32 * self.sigma2)).exp();
         let c = {
-            let left = (2.0*PI*self.sigma2).powf((self.dimensions as f32)/2.0);
-            let right = self.w/(1.0-self.w)*(self.num_source_points as f32)/(self.num_target_points as f32);
+            let left = (2.0 * PI * self.sigma2).powf((self.dimensions as f32) / 2.0);
+            let right = self.w / (1.0 - self.w) * (self.num_source_points as f32)
+                / (self.num_target_points as f32);
             left * right
         };
         let mut den = P.sum_axis(Axis(0));
-        den = den.mapv(|v| if v == 0.0 {f32::EPSILON + c} else {v + c});
+        den = den.mapv(|v| if v == 0.0 { f32::EPSILON + c } else { v + c });
 
-        self.P = P.clone()/den;
+        self.P = P.clone() / den;
         self.Pt1 = self.P.clone().sum_axis(Axis(0));
         self.P1 = self.P.clone().sum_axis(Axis(1));
         self.Np = self.P1.sum();
@@ -136,12 +139,20 @@ impl CoherentPointDriftTransform {
         self.W = solve_matrices(&A, &B);
     }
 
-    fn update_variance(&mut self) { 
+    fn update_variance(&mut self) {
         let qprev = self.sigma2;
         self.q = f32::MAX;
-        let xPx = self.Pt1.clone().t().dot(&self.X.clone().powi(2).sum_axis(Axis(1)));
-        let yPy = self.P1.clone().t().dot(&self.TY.clone().powi(2).sum_axis(Axis(1)));
-        let trPXY = (self.TY.clone()*self.PX.clone()).sum();
+        let xPx = self
+            .Pt1
+            .clone()
+            .t()
+            .dot(&self.X.clone().powi(2).sum_axis(Axis(1)));
+        let yPy = self
+            .P1
+            .clone()
+            .t()
+            .dot(&self.TY.clone().powi(2).sum_axis(Axis(1)));
+        let trPXY = (self.TY.clone() * self.PX.clone()).sum();
 
         self.sigma2 = (xPx - 2.0 * trPXY + yPy) / (self.Np * self.dimensions as f32);
         if self.sigma2 <= 0.0 {
@@ -151,44 +162,39 @@ impl CoherentPointDriftTransform {
     }
 }
 
-
 // Non cpd functions.
 fn compute_diff(
     X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
-    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>
+    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
 ) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> {
-    let X_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> = Array::from_shape_vec(
-        (1, X.dim().0, X.dim().1),
-        X.clone().into_raw_vec()
-    ).unwrap(); 
-    let Y_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> = Array::from_shape_vec(
-        (Y.dim().0, 1, Y.dim().1),
-        Y.clone().into_raw_vec()
-    ).unwrap();
+    let X_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> =
+        Array::from_shape_vec((1, X.dim().0, X.dim().1), X.clone().into_raw_vec()).unwrap();
+    let Y_3d: ArrayBase<OwnedRepr<f32>, Dim<[usize; 3]>> =
+        Array::from_shape_vec((Y.dim().0, 1, Y.dim().1), Y.clone().into_raw_vec()).unwrap();
     X_3d - Y_3d
 }
 
 fn gaussian_kernel(
     X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
     Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
-    beta: f32
+    beta: f32,
 ) -> ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>> {
     let X = X.clone();
     let Y = Y.clone();
     let diff = compute_diff(&X, &Y).powi(2).sum_axis(Axis(2));
-    (-diff/(2.0 * beta.powi(2))).exp()
+    (-diff / (2.0 * beta.powi(2))).exp()
 }
 
 fn initialize_sigma2(
     X: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
-    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>
+    Y: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>,
 ) -> f32 {
     let num_target_points = X.dim().0 as f32;
     let dimensions = X.dim().1 as f32;
     let num_source_points = Y.dim().0 as f32;
     let diff = compute_diff(X, Y);
     let err = diff.powi(2);
-    err.sum()/(dimensions*num_target_points*num_source_points)
+    err.sum() / (dimensions * num_target_points * num_source_points)
 }
 
 fn array_to_string(arr: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>) -> String {
@@ -198,7 +204,10 @@ fn array_to_string(arr: &ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>>) -> String {
     let mut p_str: Vec<String> = Vec::new();
     for chunk in &chunks {
         let p: Vec<f32> = chunk.collect::<Vec<f32>>();
-        p_str.push(String::from(format!("{{\"x\": {}, \"y\": {}}}", p[0], p[1])));
+        p_str.push(String::from(format!(
+            "{{\"x\": {}, \"y\": {}}}",
+            p[0], p[1]
+        )));
     }
     let p_str: String = p_str.join(", ");
     arr_str.push_str(&p_str);
