a1f28d2bbab0fe5fe02e5e4d6e4f99908ae04757
Ryan Folks <rfolks92@gmail.com>
2025-05-01 10:54:02 -0400

    Fill out the medication and fluid section tuple structs and add a seperate struct for the intraoperative and preoperative/postoperative chart.

--- src/digitization/chart.rs
+++ src/digitization/chart.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+
+
+/// An enum for single digit positive whole numbers.
 enum SingleDigit {
     Zero = 0,
     One = 1,
@@ -11,5 +15,40 @@ enum SingleDigit {
     Nine = 9,
 }
 
-struct DosingRecord ()
-struct Chart { }
+/// A drug or fluid code is a three digit number.
+struct Code (SingleDigit, SingleDigit, SingleDigit);
+
+/// Contains the Code for the drug or fluid, along with a HashMap mapping the
+/// timestamp to the dose.
+struct DosingRecord ( Code, HashMap<String, u32> );
+
+
+/// Contains all 9 rows of the medications section.
+struct MedicationSection ( 
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>,
+    Option<DosingRecord>
+);
+
+/// Contains the 2 rows of the fluid/blood product section.
+struct FluidBloodProductSection ( Option<DosingRecord>, Option<DosingRecord> );
+
+/// A struct containing all of the intraoperative chart's data.
+struct IntraoperativeChart {
+    /// Which intraoperative page we are on. Some surgeries span multiple pages.
+    page_num: u32,
+    medications: Vec<DosingRecord>
+}
+
+/// A stuct containing all of the preoperative/postoperative chart's data.
+
+struct Chart {
+    intraoperative_charts: Vec<IntraoperativeChart>,
+    preoperative_postoperative_chart: PreoperativePostoperativeChart
+}
