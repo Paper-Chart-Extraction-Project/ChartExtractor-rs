a9f84b8fe920ada8407257ab7cda7c9a1cdce73f
Ryan Folks <rfolks92@gmail.com>
2025-05-02 16:35:38 -0400

    Add fn to run a yolov11 model based on the parameters.

--- src/digitization/digitize.rs
+++ src/digitization/digitize.rs
@@ -1,15 +1,17 @@
 use crate::annotations::detection::Detection;
 use crate::annotations::bounding_box::BoundingBox;
 use crate::digitization::chart::Chart;
+use crate::image_utils::image_conversion::convert_rgb_image_to_owned_array;
 use crate::image_utils::image_io::read_image_as_array4;
-use crate::image_utils::tiling::OverlapProportion;
-use crate::object_detection::object_detection_utils::tile_and_predict;
+use crate::image_utils::tiling::{OverlapProportion, pad_image_to_fit_tiling_params};
+use crate::object_detection::object_detection_utils::{read_classes_txt_file, tile_and_predict};
 use crate::object_detection::yolov11_bounding_box::Yolov11BoundingBox;
-use ndarray::{ArrayBase, Dim, ViewRepr};
+use ndarray::{ArrayBase, Dim, OwnedRepr};
 use std::path::Path;
 
 
 struct BoundingBoxModelParameters {
+    name: String,
     model_path: Box<Path>,
     class_names_path: Box<Path>,
     input_width: usize,
@@ -36,8 +38,34 @@ pub fn digitize(
 }
 
 fn run_yolov11_bounding_box_model(
-    image: ArrayBase<ViewRepr<&f32>, Dim<[usize; 4]>>,
+    image: ArrayBase<OwnedRepr<f32>, Dim<[usize; 4]>>,
     model_parameters: BoundingBoxModelParameters,
+    use_adaptive_padding: bool,
 ) -> Vec<Detection<BoundingBox>> {
-    vec![]
+    let class_names: Vec<String> = read_classes_txt_file(
+        &model_parameters.class_names_path
+    ).unwrap();
+    let model: Yolov11BoundingBox = Yolov11BoundingBox::new(
+        &model_parameters.model_path,
+        class_names,
+        model_parameters.input_width,
+        model_parameters.input_height,
+        model_parameters.name
+    ).unwrap();
+    if use_adaptive_padding {
+        let image = pad_image_to_fit_tiling_params(
+            &image,
+            model_parameters.tile_size,
+            model_parameters.overlap_proportion,
+        );
+        let image = convert_rgb_image_to_owned_array(image);
+    }
+    tile_and_predict(
+        &model,
+        image,
+        model_parameters.tile_size,
+        model_parameters.overlap_proportion,
+        model_parameters.confidence_threshold,
+        model_parameters.nms_threshold
+    ).unwrap()
 }
