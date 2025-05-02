60cdd5318fdf85f8df0ee6acf5a9e39fbc802adf
Ryan Folks <rfolks92@gmail.com>
2025-05-02 15:53:28 -0400

    Add a new struct to hold all of the digitization parameters.
    Eventually there will be a method to create one by reading Json.

--- src/digitization/digitize.rs
+++ src/digitization/digitize.rs
@@ -1,10 +1,20 @@
+use crate::annotations::detection::Detection;
+use crate::annotations::bounding_box::BoundingBox;
 use crate::digitization::chart::Chart;
+use crate::image_utils::image_io::read_image_as_array4;
 use std::path::Path;
 
 
+struct DigitzationParameters { }
+
 pub fn digitize(
     preop_postop_image_filepath: &Path,
-    intraop_image_filepath: &Path
+    intraop_image_filepath: &Path,
+    parameters: DigitzationParameters
 ) -> Result<Chart, &'static str> {
+    let preop_postop_image = read_image_as_array4(preop_postop_image_filepath);
+    let intraop_image = read_image_as_array4(intraop_image_filepath);
+
     Err("")
 }
+
