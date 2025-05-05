62d6a906faba66e4f9b16a0b9aefc2f820f2d974
RyanDoesMath <rfolks92@gmail.com>
2025-05-05 11:29:04 -0400

    Add a function to read the centroids from a json file.

--- src/digitization/mod.rs
+++ src/digitization/mod.rs
@@ -1,2 +1,3 @@
 pub mod chart;
 pub mod digitize;
+pub mod read_centroids;
---
+++ src/digitization/read_centroids.rs
@@ -0,0 +1,25 @@
+use crate::annotations::point::Point;
+use serde_json::{from_reader, Value};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+
+pub fn read_centroids_from_json(filepath: &Path) -> HashMap<String, Point> {
+    let file = File::open(filepath).unwrap();
+    let reader = BufReader::new(file);
+    let mut centroids: HashMap<String, Point> = HashMap::new();
+    let centroids_json: serde_json::Value = serde_json::from_reader(reader).unwrap();
+    if let serde_json::Value::Object(map) = centroids_json {
+        for (key, value) in map.iter() {
+            let name: String = key.as_str().to_string();
+            let centroid: Point = Point {
+                x: value[0].as_f64().unwrap() as f32,
+                y: value[1].as_f64().unwrap() as f32
+            };
+            centroids.insert(name, centroid);
+        }
+    }
+    centroids
+}
